40b594df5da4d1e2725042a9d4e5bfa965e6bc40
Wasiul Islam <winafiu@gmail.com>
2022-01-06 12:44:35 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="flex justify-between p-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
+      <nav>
+        <ul className="flex gap-[1rem]">
+          <li className="link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="link">
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li className="link">
+            <Link to="/login">Login</Link>
+          </li>
+          <li className="link">
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  )
 }
 
-export default App;
+export default App
