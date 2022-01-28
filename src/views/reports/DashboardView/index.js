2b5f7d3013e56c927549fca6ea8cc4e5a836a925
Wasiul Islam <winafiu@gmail.com>
2022-01-29 00:44:43 +0600

    added game

--- src/views/reports/DashboardView/index.js
+++ src/views/reports/DashboardView/index.js
@@ -1,74 +1,62 @@
-import React from 'react';
-import {
-  Container,
-  Grid,
-  makeStyles
-} from '@material-ui/core';
-import Page from './../../../components/Page';
-import Header from './Header';
-import ActiveUsers from './ActiveUsers';
+import React from 'react'
+import { Container, Grid, makeStyles } from '@material-ui/core'
+import Page from './../../../components/Page'
+import Header from './Header'
+import ActiveUsers from './ActiveUsers'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     minHeight: '100%',
     paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3)
+    paddingBottom: theme.spacing(3),
   },
   container: {
     [theme.breakpoints.up('lg')]: {
       paddingLeft: 64,
-      paddingRight: 64
-    }
-  }
-}));
+      paddingRight: 64,
+    },
+  },
+}))
 
 function DashboardView() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
-    <Page
-      className={classes.root}
-      title="Dashboard"
-    >
-      <Container
-        maxWidth={false}
-        className={classes.container}
-      >
+    <Page className={classes.root} title="Dashboard">
+      <Container maxWidth={false} className={classes.container}>
         <Header />
-        <Grid
-          container
-          spacing={3}
-        >
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xs={12}
-          >
+        <Grid container spacing={3}>
+          <Grid item lg={3} sm={6} xs={12}>
             <ActiveUsers title={'Child'} value={34} />
           </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <ActiveUsers title={'Volunteer'} value={34}/>
+          <Grid item lg={3} sm={6} xs={12}>
+            <ActiveUsers title={'Volunteer'} value={34} />
           </Grid>
-          <Grid
-            item
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <ActiveUsers title={'Organization'} value={34}/>
+          <Grid item lg={3} sm={6} xs={12}>
+            <ActiveUsers title={'Organization'} value={34} />
           </Grid>
-          
         </Grid>
+
+        <iframe
+          height={546}
+          style={{ width: '100%', marginTop: '2rem' }}
+          scrolling="no"
+          title="React Game- Elephant Taco Hunt"
+          src="https://codepen.io/sdras/embed/YWBdQd?default-tab=result"
+          frameBorder="no"
+          loading="lazy"
+          allowTransparency="true"
+          allowFullScreen="true"
+        >
+          See the Pen &lt;a href="https://codepen.io/sdras/pen/YWBdQd"&gt; React
+          Game- Elephant Taco Hunt&lt;/a&gt; by Sarah Drasner (&lt;a
+          href="https://codepen.io/sdras"&gt;@sdras&lt;/a&gt;) on &lt;a
+          href="https://codepen.io"&gt;CodePen&lt;/a&gt;.
+        </iframe>
       </Container>
     </Page>
-  );
+  )
 }
 
-export default DashboardView;
+export default DashboardView
